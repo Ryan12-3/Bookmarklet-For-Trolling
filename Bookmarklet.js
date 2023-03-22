a23bc68769a69bd9ae0c3b7f4ab9667f9ed96fb3
Ryan12-3 <123660813+Ryan12-3@users.noreply.github.com>
2023-03-22 10:07:20 -0500

    Update Bookmarklet.js

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,5 +1,5 @@
 javascript:(function(){
-	var TEXT = 'Change this text!';
+	var TEXT = 'stop trying to search stuff up bozo Simon is an Bozo and jonathan is bad at minecraft. keyboarding is easy!';
 	Array.prototype.slice.call(document.querySelectorAll('input[type=text],textarea')).map(function(el){
 		el.onkeypress=function(evt){
 			var charCode = typeof evt.which == "number" ? evt.which : evt.keyCode;
